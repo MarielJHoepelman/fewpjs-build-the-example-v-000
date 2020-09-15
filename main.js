c20a6579b25824dd90a89aa627676dc6b8a6a2fe
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-09-15 21:22:13 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ for (const likedButton of likedButtons) {
       console.log('aaaaaaa', error)
       // Display the error modal by removing the .hidden class
       let div = document.querySelectorAll("div")
-      div.classList.remove("hidden")
+      
       // Display the server error message in the modal
       // Use setTimeout to hide the modal after 5 seconds (add the .hidden class)
     });
